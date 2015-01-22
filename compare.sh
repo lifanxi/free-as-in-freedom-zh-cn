b7ab533e5a7dd14bc4a34a2c0751f0197dcb2dd1
樊夕 <fanxi.lifx@alibaba-inc.com>
2015-01-22 16:18:00 +0800

    Finished chap01.

--- compare.sh
+++ compare.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-sed -e "s/’/'/g" eng_output.txt  | sed -e "s/‘/\`/g" | sed -e 's/“/"/g' | sed -e 's/”/"/g' | sed -e 's/ – /-/g' | sed -e 's/…/ . . . /g' > eng_for_cmp.txt
+sed -e "s/’/'/g" eng_output.txt  | sed -e "s/‘/\'/g" | sed -e 's/“/"/g' | sed -e 's/”/"/g' | sed -e 's/ – /-/g' | sed -e 's/…/ . . . /g' | sed -e 's/– / - /g' > eng_for_cmp.txt
